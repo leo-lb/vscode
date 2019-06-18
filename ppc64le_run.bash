7445e2d3d828d5a80b48d35c0c7ecbb4279a1c60
Leo Le Bouter <lle-bout@zaclys.net>
2019-06-18 14:22:23 +0200

    Tell a little more about building Electron from source.

--- ppc64le_run.bash
+++ ppc64le_run.bash
@@ -6,6 +6,9 @@ if [[ "$ELECTRON_BUILD_DIR" == "" ]]; then
   echo "ELECTRON_BUILD_DIR environment variable not provided, downloading prebuilt electron.."
 	echo "The prebuilt electron binary is NOT static linked, so make sure to install it's dependencies."
 	echo "For Ubuntu Bionic, the build dependencies are: build-essential clang git vim cmake python libcups2-dev pkg-config libnss3-dev libssl-dev libglib2.0-dev libgnome-keyring-dev libpango1.0-dev libdbus-1-dev libatk1.0-dev libatk-bridge2.0-dev libgtk-3-dev libkrb5-dev libpulse-dev libxss-dev re2c subversion curl libasound2-dev libpci-dev mesa-common-dev gperf bison uuid-dev clang-format libatspi2.0-dev libnotify-dev libgconf2-dev libcap-dev libxtst-dev libxss1 python-dbusmock openjdk-8-jre ninja-build clang-format"
+  echo
+  echo "If you want to build Electron yourself, vscode needs the version 4.2.4, you can look at https://github.com/leo-lb/electron/blob/electron-ppc64le-4.2.4/README.md for instructions."
+  echo "You can then rerun this script with ELECTRON_BUILD_DIR defined. e.g ELECTRON_BUILD_DIR=/path/to/electron-build-script-working-dir/electron-gn/src/out/Release ./ppc64le_run.bash"
   echo "#############################"
   echo "#############################"
 	echo
